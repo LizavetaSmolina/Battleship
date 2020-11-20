341576bc929e56af8842780daebe072d37f602f6
Елизавета Смолина <lizasmolina0211@gmail.com>
2020-11-20 10:21:43 +0100

    Naprawianie błedów

--- Battleship/functions.h
+++ Battleship/functions.h
@@ -13,17 +13,4 @@ int finish_game(int*);
 int shot(int*, int, int, int*);
 void game(char*, char*, int*, int*, int*, int*);
 
-/*char* new_field();
-int* new_int_array();
-int new_ship(int* field, enum direction dir, enum type_of_ship type, int x, int y);
-int letter_to_int(char x);
-int* init_field(int* ships);
-int* init_field_random(int* ships);
-void print_field(char* field, int* ships);
-void print_fields(char* user_field, char* computer_field, int* user_ships, int* computer_ships);
-int finish_game(int* field);
-int shot(int* ships, int x, int y, int* hits);
-void game(char* user_field, char* computer_field, int* user_ships, int* computer_ships, int* user_hits, int* computer_hits)*/
-
-
 #endif
